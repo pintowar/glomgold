edb6645c1a884c9bd3ed2eec1f71c971a9391687
Thiago Oliveira <pintowar@gmail.com>
2022-03-12 21:52:52 -0300

    changing database seed (development)

--- server/src/main/kotlin/com/github/pintowar/conf/SeedInitializer.kt
+++ server/src/main/kotlin/com/github/pintowar/conf/SeedInitializer.kt
@@ -8,16 +8,12 @@ import io.micronaut.context.annotation.Requires
 import io.micronaut.runtime.event.annotation.EventListener
 import io.micronaut.runtime.server.event.ServerStartupEvent
 import jakarta.inject.Singleton
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import mu.KLogging
 import java.math.BigDecimal
-import java.time.Duration
 import java.time.YearMonth
+import java.time.ZoneId
 import java.util.*
 
 @Requires(property = "micronaut.environments", value = "dev")
@@ -31,57 +27,59 @@ class SeedInitializer(
     fun onStartUp(e: ServerStartupEvent) {
         logger.info("starting data initialization at StartUpEvent: $e")
         runBlocking {
-            if (userRepo.findByUsername("admin") == null) {
-                val user = User(
-                    username = "admin",
-                    name = "Administrator",
-                    email = "admin@glomgold.com",
-                    admin = true
-                ).apply { setPassword("admin") }
-                val admin = userRepo.save(user)
-
-                val data = items(admin)
-                itemRepo.saveAll(data)
-                    .onEach { logger.debug("saved item: $it") }
-                    .onCompletion { logger.debug("completed.") }
-                    .flowOn(Dispatchers.IO)
-                    .launchIn(this)
-            }
-            logger.info("data initialization is done...")
+            val allItems = genUsers()
+                .filter { userRepo.findByUsername(it.username) == null }
+                .map { userRepo.save(it) }
+                .filterNot { it.admin }
+                .flatMap { user ->
+                    val items = genItems(user)
+                    itemRepo.saveAll(items).toList()
+                }
+            logger.info("Data initialization is done!! Generated ${allItems.size} items.")
         }
     }
 
-    private fun items(user: User) = listOf(
-        Item(
-            description = "Water",
-            value = BigDecimal(10),
-            period = YearMonth.now(),
-            userId = user.id!!
-        ),
-        Item(
-            description = "Groceries",
-            value = BigDecimal(200),
-            period = YearMonth.now(),
-            userId = user.id!!
-        ),
-        Item(
-            description = "Clothing",
-            value = BigDecimal(500),
-            period = YearMonth.now(),
-            userId = user.id!!
-        ),
-        Item(
-            description = "Groceries",
-            value = BigDecimal(150),
-            period = YearMonth.now().plusMonths(1),
-            userId = user.id!!
-        ),
-        Item(
-            description = "Clothing",
-            value = BigDecimal(300),
-            period = YearMonth.now().plusMonths(1),
-            userId = user.id!!
-        )
+    private fun genUsers() = listOf(
+        User(
+            username = "admin",
+            name = "Administrator",
+            email = "admin@glomgold.com",
+            admin = true
+        ).apply { setPassword("admin") },
+        User(
+            username = "scrooge",
+            name = "Scrooge McDuck",
+            email = "scrooge@glomgold.com",
+        ).apply { setPassword("123123") },
+        User(
+            username = "donald",
+            name = "Donald Duck",
+            email = "donald@glomgold.com",
+            locale = Locale("pt", "BR"),
+            timezone = ZoneId.of("America/Fortaleza")
+        ).apply { setPassword("123123") }
     )
+
+    private fun genItems(user: User): List<Item> {
+        val items = listOf(
+            Pair("Furniture", 10),
+            Pair("Gym", 100),
+            Pair("Condominium", 4000),
+            Pair("Groceries", 4000),
+            Pair("Clothing", 500),
+            Pair("Transport", 500),
+            Pair("Health", 2500),
+            Pair("Food", 2000),
+            Pair("Nightlife", 700),
+            Pair("Security", 800),
+            Pair("Presents", 300),
+            Pair("Other services", 800),
+        )
+        return (0..3).flatMap { extraMonth ->
+            items.filter { Math.random() >= 0.3 }.map { (desc, value) ->
+                Item(desc, BigDecimal(value), YearMonth.now().plusMonths(extraMonth.toLong()), user.id!!)
+            }
+        }
+    }
 
 }
