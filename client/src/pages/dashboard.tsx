82f5e50d5542fe6aac32136ad3820d1a77e14c38
Thiago Oliveira <pintowar@gmail.com>
2022-03-16 01:14:28 -0300

    git info on admin dashboard

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,39 +1,37 @@
-import { useGetIdentity, usePermissions } from "@pankod/refine-core";
+import { useCustom } from "@pankod/refine-core";
 
-import { Row, Col, Card, Avatar, Typography, Space } from "@pankod/refine-antd";
+import { Row, Col, Card, Typography, Space } from "@pankod/refine-antd";
+import { useMemo } from "react";
 
 const { Text } = Typography;
 
 export const DashboardPage: React.FC = () => {
-    const { data: identity } =
-        useGetIdentity<{
-            id: string;
-            name: string;
-            avatar: string;
-        }>();
-    const permissions = usePermissions();
+    
+    const { data } = useCustom({
+        url: `/management/info`,
+        method: "get",
+    });
+
+    const appInfo = useMemo(() => {
+        const branch = data?.data?.git?.branch;
+        const version = data?.data?.git?.build?.version;
+        return {branch, version}
+    }, [data]);
 
     return (
         <Row gutter={20}>
             <Col span={6}>
                 <Card
-                    title="Identity"
+                    title="Management Info"
                     style={{ height: "300px", borderRadius: "15px" }}
                     headStyle={{ textAlign: "center" }}
                 >
                     <Space align="center" direction="horizontal">
-                        <Avatar size="large" src={identity?.avatar} />
-                        <Text>{identity?.name}</Text>
+                        <ul>
+                            <li>Branch: {appInfo.branch}</li>
+                            <li>Version: {appInfo.version}</li>
+                        </ul>
                     </Space>
-                </Card>
-            </Col>
-            <Col span={6}>
-                <Card
-                    title="Permissions"
-                    style={{ height: "300px", borderRadius: "15px" }}
-                    headStyle={{ textAlign: "center" }}
-                >
-                    <Text>{permissions.data}</Text>
                 </Card>
             </Col>
         </Row>
