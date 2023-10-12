a2136c9a1d340964cfdbe0d4ed692a147c2b7782
Thiago Oliveira <pintowar@gmail.com>
2023-10-12 18:21:37 -0300

    attempt to stabilize ci cypress tests

--- glomgold-webclient/cypress/e2e/panel/report.cy.ts
+++ glomgold-webclient/cypress/e2e/panel/report.cy.ts
@@ -8,7 +8,9 @@ describe("Panel Report Tests", () => {
 
     it("Report: BALANCE / 2023", () => {
       cy.intercept("POST", "/api/login", { fixture: "login/common.user.json" });
-      cy.intercept("GET", "/api/panel/report?&type=BALANCE&year=2023", { fixture: "panel/report.json" });
+      cy.intercept("GET", "/api/panel/report?&type=BALANCE&year=2023", { fixture: "panel/report.json" }).as(
+        "currentReport"
+      );
 
       const { username, password } = user;
 
@@ -20,14 +22,16 @@ describe("Panel Report Tests", () => {
 
       cy.url().should("include", "/panel/report");
 
-      cy.get(`[data-row-key="1"] > :nth-child(10) a`).should("have.css", "color", "rgb(245, 34, 45)");
+      cy.wait("@currentReport").then(() => {
+        cy.get(`[data-row-key="1"] > :nth-child(10) a`).should("have.css", "color", "rgb(245, 34, 45)");
 
-      cy.get(`[data-row-key="2"] > :nth-child(9) > a`).should("have.css", "color", "rgb(22, 119, 255)");
+        cy.get(`[data-row-key="2"] > :nth-child(9) > a`).should("have.css", "color", "rgb(22, 119, 255)");
 
-      cy.get(`[data-node-key="2"]`).click();
+        cy.get(`[data-node-key="2"]`).click();
 
-      cy.get("#apexchartsline").should("have.css", "height", "350px");
-      cy.get("#apexchartsbar").should("have.css", "height", "350px");
+        cy.get("#apexchartsline").should("have.css", "height", "350px");
+        cy.get("#apexchartsbar").should("have.css", "height", "350px");
+      });
     });
   });
 });
